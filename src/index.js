8a76191533336ba85bdc30e258210dc4d8d57022
Vishal Kandu <kanduvishal07@gmail.com>
2024-03-05 22:29:07 +0530

    wrapper is created

--- src/index.js
+++ src/index.js
@@ -10,7 +10,14 @@ dotenv.config({
 
 
 connectDB()
-
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(` Server is running on port: ${process.env.PORT}`);
+    })
+})
+.catch((err) => {
+    console.log("MONGO db connection failed !!!", err);
+})
 
 
 
